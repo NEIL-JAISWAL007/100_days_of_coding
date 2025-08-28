1e42e0558715829aec9a0a12a538bcc95120cb44
NEIL-JAISWAL007 <neiljaiswal09@gmail.com>
2025-08-28 13:35:21 +0530

    100 days of coding

---
+++ 01_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("your number 1 is ");
+    scanf("%D", &a);
+    printf(" your number 2  is ");
+    scanf("%D", &b);
+    printf("sum is %d", a + b);
+    return 0;
+}
---
+++ 02_Operation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    float a;
+    float b;
+    printf("enter your first number ");
+    scanf("%f", &a);
+    printf("enter your second number ");
+    scanf("%f", &b);
+    printf("sum is %f\n", a + b);
+    printf("difference is %f\n", a-b);
+    printf("product is %f\n", a*b);
+    printf("quotient  is %f\n", a/b);
+
+    return 0.0;
+}
