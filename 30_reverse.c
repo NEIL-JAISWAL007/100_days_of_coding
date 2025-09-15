045e1b9082d89b862393b8b4144ad82325993506
NEIL-JAISWAL007 <neiljaiswal09@gmail.com>
2025-09-15 16:01:23 +0530

    100 days of coding

--- 30_reverse.c
+++ 30_reverse.c
@@ -1,4 +1,18 @@
-#include <stdio.h>
+/*
+Q30: Write a program to reverse a given number.
+
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/#include <stdio.h>
 int main()
 { int i = 0, n;
     printf("Enter a number: ");
