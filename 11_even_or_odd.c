9eb994db3a078af2b74c0d4361d510426dc3b8f0
NEIL-JAISWAL007 <neiljaiswal09@gmail.com>
2025-09-15 15:45:59 +0530

    Update 11_even_or_odd.c

--- 11_even_or_odd.c
+++ 11_even_or_odd.c
@@ -1,4 +1,18 @@
-#include <stdio.h>
+/*
+Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/#include <stdio.h>
 int main()
 {
     int a;
