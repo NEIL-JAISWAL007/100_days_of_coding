3676dfe63785f438ab1136c5f4be66a146719154
NEIL-JAISWAL007 <neiljaiswal09@gmail.com>
2025-09-15 15:50:53 +0530

    100 days of coding

--- 16_three_numbers.c
+++ 16_three_numbers.c
@@ -1,4 +1,18 @@
-#include <stdio.h>
+/*
+Q16: Write a program to input three numbers and find the largest among them using if–else.
+
+Sample Test Cases:
+Input 1:
+3 7 5
+Output 1:
+Largest is 7
+
+Input 2:
+-1 -5 0
+Output 2:
+Largest is 0
+
+*/#include <stdio.h>
 int main()
 {
     int a, b, c;
