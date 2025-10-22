4cac9dd5fef1b303536caafbc7389d07e750bf24
NEIL-JAISWAL007 <neiljaiswal09@gmail.com>
2025-10-22 18:45:09 +0530

    100 Days of coding

---
+++ 41_number_swap.c
@@ -0,0 +1,31 @@
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4231
+
+Input 2:
+1001
+Output 2:
+1001
+
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, firstDigit, lastDigit, digits, swappedNum;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    lastDigit = num % 10;
+    digits = (int)log10(num);
+    firstDigit = num / pow(10, digits);
+    swappedNum = lastDigit * pow(10, digits)
+                 + (num % (int)pow(10, digits)) / 10 * 10
+                 + firstDigit;
+
+    printf("Swapped number is %d", swappedNum);
+
+    return 0;
+}
